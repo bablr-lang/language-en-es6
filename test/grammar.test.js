ac92f583f5467b494aa8f1de36fc94679bdd506b
Conrad Buck <conartist6@gmail.com>
2025-05-28 10:36:08 -0600

    Initial commit

---
+++ test/grammar.test.js
@@ -0,0 +1,98 @@
+import { spam } from '@bablr/boot';
+import { dedent } from '@qnighy/dedent';
+// eslint-disable-next-line import/no-unresolved
+import * as language from '@bablr/language-en-es6';
+import { buildTag } from 'bablr';
+import { debugEnhancers } from '@bablr/helpers/enhancers';
+import { expect } from 'expect';
+import { printPrettyCSTML } from '@bablr/helpers/tree';
+import { buildIdentifier, buildString } from '@bablr/helpers/builders';
+
+let enhancers = undefined;
+
+const buildJSTag = (type) => {
+  const matcher = spam`<$${buildIdentifier(type)} />`;
+  return buildTag(language, matcher, undefined, { enhancers });
+};
+
+const print = (tree) => {
+  return printPrettyCSTML(tree.node);
+};
+
+describe('@bablr/language-en-es6', () => {
+  describe('Program', () => {
+    const js = buildJSTag('Program');
+
+    it('js`import foo from "bar"`', () => {
+      expect(print(js`import foo from "bar"`)).toEqual(dedent`\
+        <!0:cstml { bablrLanguage: 'https://bablr.org/languages/universe/es6' }>
+        <$_>
+          .:
+          <$Program>
+            body[]: []
+            body[]:
+            <$ImportDeclaration>
+              sigilToken: <*Keyword 'import' />
+              #: :Space: <*Space ' ' />
+              specifiers[]$: []
+              specifiers[]$:
+              <$ImportDefaultSpecifier>
+                local:
+                <$Identifier>
+                  value: <*Literal 'foo' />
+                </>
+              </>
+              openSpecifiersToken: null
+              closeSpecifiersToken: null
+              #: :Space: <*Space ' ' />
+              fromToken: <*Keyword 'from' />
+              #: :Space: <*Space ' ' />
+              source$:
+              <$String>
+                open: <*Punctuator '"' { balanced: '"', balancedSpan: 'String:Double' } />
+                content: <*StringContent 'bar' />
+                close: <*Punctuator '"' { balancer: true } />
+              </>
+              endToken: null
+            </>
+          </>
+        </>\n`);
+    });
+
+    it('export food, {stuff} from "bar";', () => {
+      expect(print(js`import foo from "bar"`)).toEqual(dedent`\
+        <!0:cstml { bablrLanguage: 'https://bablr.org/languages/universe/es6' }>
+        <$_>
+          .:
+          <$Program>
+            body[]: []
+            body[]:
+            <$ImportDeclaration>
+              sigilToken: <*Keyword 'import' />
+              #: :Space: <*Space ' ' />
+              specifiers[]$: []
+              specifiers[]$:
+              <$ImportDefaultSpecifier>
+                local:
+                <$Identifier>
+                  value: <*Literal 'foo' />
+                </>
+              </>
+              openSpecifiersToken: null
+              closeSpecifiersToken: null
+              #: :Space: <*Space ' ' />
+              fromToken: <*Keyword 'from' />
+              #: :Space: <*Space ' ' />
+              source$:
+              <$String>
+                open: <*Punctuator '"' { balanced: '"', balancedSpan: 'String:Double' } />
+                content: <*StringContent 'bar' />
+                close: <*Punctuator '"' { balancer: true } />
+              </>
+              endToken: null
+            </>
+          </>
+        </>\n`);
+    });
+  });
+});
