32252c66fa5d91df7d07e4b0146a3a9604d107f9
Conrad Buck <conartist6@gmail.com>
2025-11-05 13:52:14 -0700

    export default

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -1,7 +1,7 @@
 import { spam } from '@bablr/boot';
 import { dedent } from '@qnighy/dedent';
 // eslint-disable-next-line import/no-unresolved
-import * as language from '@bablr/language-en-es6';
+import language from '@bablr/language-en-es6';
 import { buildTag } from 'bablr';
 import { debugEnhancers } from '@bablr/helpers/enhancers';
 import { expect } from 'expect';
